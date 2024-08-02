b0becbc2a6c63d6fa00aac04090acccd467fab8b
Akash7016 <yadavakash9374@gmail.com>
2024-08-02 02:23:47 -0400

    added preloader, audio and video, favicon

---
+++ js/script.js
@@ -0,0 +1,5 @@
+setTimeout(function() {
+    document.getElementById('preloader').style.opacity = '0';
+    document.getElementById('preloader').style.visibility = 'hidden';
+    document.getElementById('content').style.display = 'block';
+}, 1500); 
